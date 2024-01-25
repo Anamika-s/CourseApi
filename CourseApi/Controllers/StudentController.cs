558deb239d37cfb522cb29df6bb8994e97f8e49e
Anamika-s <anamika.sawhney22@gmail.com>
2024-01-25 11:47:52 +0530

    Add project files.

---
+++ CourseApi/Controllers/StudentController.cs
@@ -0,0 +1,63 @@
+﻿using CourseApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        static List<Student> list = null;
+        public StudentController() {
+            if (list == null)
+            {
+                list = new List<Student>()
+                {
+                    new Student() { Id = 1, Name = "Ajay", Marks = 90 },
+
+                    new Student() { Id = 2, Name = "Ajay", Marks = 90 },
+
+                    new Student() { Id = 3, Name = "Ajay", Marks = 90 },
+
+                    new Student() { Id = 4, Name = "Ajay", Marks = 90 }
+                };
+            }
+        }
+        public List<Student> Get()
+        {
+                return list;    
+          }
+
+        [HttpGet]
+        [Route("{id}")]
+        public  Student GetById(int id)
+        {
+            return list.FirstOrDefault(x => x.Id == id); ;
+        }
+        [HttpPost]
+        public void AddRecord(Student student)
+        {
+            list.Add(student);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public void EditStudent(int id, Student student)
+        {
+
+        }
+
+        [HttpDelete("{id}")]
+
+        public void DeleteStduent(int id)
+        {
+            var obj = GetById(id);
+            if (obj != null)
+
+                list.Remove(obj);   
+        }
+
+
+    }
+}
